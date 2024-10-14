2653480eff8bdc3dd7eef40a54eee48d34dacd32
Swapnil Sen <swapnil.sen2013@gmail.com>
2024-10-14 17:58:38 +0530

    Update sample_script.py

--- sample_script.py
+++ sample_script.py
@@ -6,10 +6,15 @@ def main():
         sys.exit(1)
     
     script_name = sys.argv[0]
-    input_param = sys.argv[1]
+    central_api_token = sys.argv[1]
+    serial_num = sys.argv[2]
+    device_type = sys.argv[3]
 
     print(f"Script Name: {script_name}")
-    print(f"Input Parameter: {input_param}")
+    print(f"Central API Token: {central_api_token}")
+    print(f"Serial Number: {central_api_token}")
+    print(f"Device Type: {central_api_token}")
+    
 
 if __name__ == "__main__":
     main()
